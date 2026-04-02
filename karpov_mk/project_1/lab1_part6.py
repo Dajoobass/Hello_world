bfc7e90185b807427847b34d372b8303895bd6a8
Dajoobass <dajoobass@outlook.com>
2026-04-02 02:37:57 +0000

    Add files via upload

---
+++ karpov_mk/project_1/lab1_part6.py
@@ -0,0 +1,3 @@
+f = open("c:/Users/Dajoobass/Documents/Академические работы/Infa_2sem/karpov_mk/project_2/output.txt", "w", encoding="utf-8")
+print(f"Фамилия: Карпов\n","Имя: Михаил\n","Группа:", 471903,"/",50001, "\nГод поступления:", 2025, file=f)
+f.close()
